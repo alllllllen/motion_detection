8b52c7eea29efd9d9180c61fbaeebf636091b9d5
Unknown <linux17@gmail.com>
2018-05-23 00:48:54 +0800

    change data communication method from topic to firebase

--- src/motion_detector.py
+++ src/motion_detector.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 import os
-import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 import rospy
 import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from std_msgs.msg import String ,Bool
-from motion_detection.msg import TableStatus
-from motion_detection.msg import Tables
 
-
-info_of_table = {
-}
-
-tables = Tables()
+path = os.path.dirname(__file__)
+cred = credentials.Certificate(path+'/serviceAccount.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 class Motion_detector:
-    def __init__(self, table, tableStatus):
-        self.pub = rospy.Publisher("table/info_"+table, TableStatus, queue_size=2)
+    def __init__(self, table):
+        # self.pub = rospy.Publisher("table/info_"+table['tablename'], TableStatus, queue_size=2)
         self.table = table
-        self.tableStatus = tableStatus
-        # self.pub = rospy.Publisher("table/info_tables", Tables, queue_size=2)        
-    def detect(self,image):
+        self.path = 'Tables/'+self.table['tablename']
+        self.update_info_of_table = db.document(self.path)
+        self.count_false = 0
+        self.count_true = 0
+    def detect(self, image):
         blur = cv2.blur(image, (4, 4))
         try:
             diff = cv2.absdiff(self.avg, blur)
@@ -38,52 +38,52 @@ class Motion_detector:
         _cntImg, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for c in cnts:
             if cv2.contourArea(c) < 500:
-                self.tableStatus.ins = False
-                self.pub.publish(self.tableStatus)
+                # self.count_false = self.count_false+1
+                # if self.count_false > 5:
+                #     tmp = {'ins': False}
+                #     self.update_info_of_table.update(tmp)
+                #     self.count_false = 0
+                tmp = {'ins': False}
+                self.update_info_of_table.update(tmp)
                 continue
-            self.tableStatus.ins = True
-            self.pub.publish(self.tableStatus)
+            tmp = {'ins': True}
+            self.update_info_of_table.update(tmp)
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # cv2.drawContours(image, cnts, -1, (0, 255, 255), 2)
         cv2.accumulateWeighted(blur, self.avg_float, 0.01)
         self.avg = cv2.convertScaleAbs(self.avg_float)
         return image
+
 class Motion:
-    def __init__(self, table, tableStatus):
+    def __init__(self, table):
         self.table = table
-        self.tableStatus = tableStatus
         self.bridge = CvBridge()
-        self.pub = rospy.Publisher("camera/visible/image_"+self.table, Image, queue_size=2)  
+        self.pub = rospy.Publisher("camera/visible/image_"+self.table['tablename'], Image, queue_size=2)  
         rospy.Subscriber("usb_cam/image_raw", Image, self.imageCallback)
-        self.motion_detector = Motion_detector(self.table, tableStatus)
-        # rospy.spin()
+        self.motion_detector = Motion_detector(self.table)
     def imageCallback(self, image):
         if self.motion_detector:
             cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
-            result_img = self.motion_detector.detect(cv_image[info_of_table[self.table]["row0"]:info_of_table[self.table]["row1"], info_of_table[self.table]["col0"]:info_of_table[self.table]["col1"]])
+            # result_img = self.motion_detector.detect(cv_image[info_of_table[self.table]["row0"]:info_of_table[self.table]["row1"], info_of_table[self.table]["col0"]:info_of_table[self.table]["col1"]])
+            result_img = self.motion_detector.detect(cv_image[self.table["col0"]:self.table["col1"], self.table["row0"]:self.table["row1"]])
             image = self.bridge.cv2_to_imgmsg(result_img, "bgr8")
         self.pub.publish(image)
 
+def shutdown():
+    info_of_tables = db.collection('Tables')
+    info_of_tables = info_of_tables.get()
+    for table in info_of_tables:
+        table = table.to_dict()
+        path = 'Tables/'+table['tablename']
+        db.document(path).update({'ins': False, 'preorder': False})
+  
 if __name__ == '__main__':
     rospy.init_node("MotionDetection")
-    path = os.path.dirname(__file__)
-    with open(path+"/info_table.json", "r") as reader:
-        info_of_table = json.loads(reader.read())
-    list_of_table = info_of_table.keys()
-    for table in list_of_table:
-        tableStatus = TableStatus()
-        tableStatus.tablename = table
-        tableStatus.id = info_of_table[table]["id"]
-        tableStatus.row0 = info_of_table[table]["row0"]
-        tableStatus.row1 = info_of_table[table]["row1"]
-        tableStatus.col0 = info_of_table[table]["col0"]
-        tableStatus.col1 = info_of_table[table]["col1"]
-        tableStatus.maxnum = info_of_table[table]["maxnum"]
-        tableStatus.preorder = info_of_table[table]["preorder"]
-        tableStatus.ins = info_of_table[table]["ins"]
-        tableStatus.distance = info_of_table[table]["distance"]
-        tableStatus.priority = info_of_table[table]["priority"]
-        # tables.tables.append(tableStatus)
-        Motion(table, tableStatus)
+    info_of_tables = db.collection('Tables')
+    info_of_tables = info_of_tables.get()
+    for table in info_of_tables:
+        table = table.to_dict()
+        Motion(table)
     rospy.spin()
+    rospy.on_shutdown(shutdown)
